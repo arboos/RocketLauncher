3060f57d756012a62333d27c5f8dc3376200434d
arboos <151074202+arboos@users.noreply.github.com>
2024-04-11 22:40:26 +0300

    SoundsBaseCollection script added, sounds and sprites imported

---
+++ Assets/SoundsBaseCollection.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SoundsBaseCollection : MonoBehaviour
+{
+   
+    
+    public static SoundsBaseCollection Instance { get; private set; }
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            SceneManager.activeSceneChanged += SceneManagerOnactiveSceneChanged;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        Button[] buttons = GameObject.FindObjectsByType<Button>(FindObjectsInactive.Include, FindObjectsSortMode.None); //Add click sound to all buttons
+    }
+
+    private void SceneManagerOnactiveSceneChanged(Scene arg0, Scene arg1)
+    {
+        loseSound.Stop();
+        
+        Button[] buttons = GameObject.FindObjectsByType<Button>(FindObjectsInactive.Include, FindObjectsSortMode.None); //Add click sound to all buttons
+    }
+}
